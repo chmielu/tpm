7e4e72193ea5b99261ffd423667c54e83bad30f8
Krzysztof Socha <ksocha@ksocha.com>
2009-07-04 13:28:52 +0200

    clanwars fixes

--- misc/misc.py
+++ misc/misc.py
@@ -12,6 +12,56 @@ class ChatPage(TpmRequestHandler):
 		nickname=str(users.get_current_user()).split("@")[0]
 		self.misc_render("chat.html", nickname=nickname)
 
+class ClanwarsPage(TpmRequestHandler):
+	def get(self):
+		category = "clanwars"
+		cat = db.Query(models.Category).filter("slug =", db.Category(category)).get()
+		if not cat:
+			error(self, 500, "Administrator have to set up forum category with 'clanwars' slug.");return
+		clanwars = db.Query(models.Post).filter("category =", cat).order("date").fetch(limit=20)
+		array = {}
+		for topic in clanwars:
+			if not array.has_key(topic.topic_id):
+				splitted = topic.title.split(" - ")
+				if len(splitted) >= 2:
+					cmp = splitted[1].split(':')
+					if len(cmp) != 2:
+						cmp = splitted[1].split('-')
+
+					# if still not 2 someone is an idiot?
+					if len(cmp) != 2:
+						cmp = ["0","0"]
+
+					cwinfo = {
+						"opponent": splitted[0].split()[2],
+						"score": {
+							"class": (int(cmp[0]) > int(cmp[1])) and "win" or (cmp[0] == cmp[1]) and "draw" or "lost",
+							"int": re.sub(r"(\b|:)(0?0?0?)",
+								r'\1<span class="leadingzeros">\2</span>',
+								"%04d:%04d" % (int(cmp[0]), int(cmp[1]))),
+						},
+						"map": (len(splitted) > 2) and splitted[2] or None,
+						"gametype": (len(splitted) > 3) and splitted[3] or None,
+						"teams": (len(splitted) > 4) and splitted[4] or None,
+						"cup": (len(splitted) > 5) and splitted[5] or None,
+					}
+
+					array[topic.topic_id] = {
+						"oldest": topic,
+						"cwinfo": cwinfo,
+						"replies": 0
+					}
+			else:
+				array[topic.topic_id]["replies"] += 1
+
+		undecorated = array.values()
+		decorated = [(dict_["oldest"].date, dict_) for dict_ in undecorated]
+		decorated.sort()
+		decorated.reverse()
+		array = [dict_ for (key, dict_) in decorated]
+		clanwars = array
+		self.misc_render("clanwars.html", slug=category, clanwars=clanwars)
+
 class CodePage(TpmRequestHandler):
 	def get(self):
 		self.misc_render("code.html")
@@ -57,59 +107,9 @@ class LogoutHandler(TpmRequestHandler):
         else:
             self.redirect('/')
 
-class WarsPage(TpmRequestHandler):
-	def get(self):
-		category = "clanwars"
-		cat = db.Query(models.Category).filter("slug =", db.Category(category)).get()
-		if not cat:
-			error(self, 404);return
-		clanwars = db.Query(models.Post).filter("category =", cat).order("date").fetch(limit=20)
-		array = {}
-		for topic in clanwars:
-			if not array.has_key(topic.topic_id):
-				splitted = topic.title.split(" - ")
-				if len(splitted) == 4:
-					cmp = splitted[1].split(':')
-					if len(cmp) != 2:
-						cmp = splitted[1].split('-')
-
-					# if still not 2 someone is an idiot?
-					if len(cmp) != 2:
-						cmp = ["0","0"]
-
-					cwinfo = {
-						"opponent": splitted[0].split()[2],
-						"score": {
-							"class": (int(cmp[0]) > int(cmp[1])) and "win" or (cmp[0] == cmp[1]) and "draw" or "lost",
-							"int": re.sub(r"(\b|:)(0+)",
-								r'\1<span class="leadingzeros">\2</span>',
-								"%04d:%04d" % (int(cmp[0]), int(cmp[1]))),
-						},
-						"gametype": splitted[2],
-						"map": "wip",
-						"teams": splitted[3],
-						"cup": "wip",
-					}
-
-					array[topic.topic_id] = {
-						"oldest": topic,
-						"cwinfo": cwinfo,
-						"replies": 0
-					}
-			else:
-				array[topic.topic_id]["replies"] += 1
-
-		undecorated = array.values()
-		decorated = [(dict_["oldest"].date, dict_) for dict_ in undecorated]
-		decorated.sort()
-		decorated.reverse()
-		array = [dict_ for (key, dict_) in decorated]
-		clanwars = array
-		self.misc_render("clanwars.html", slug=category, clanwars=clanwars)
-
 application = webapp.WSGIApplication(
 [	('/chat', ChatPage),
-	('/clanwars', WarsPage),
+	('/clanwars', ClanwarsPage),
 	('/code', CodePage),
 	('/feed', FeedPage),
 	('/help', HelpPage),
