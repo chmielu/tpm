ff37b9107eb14131bb3728d492c465de67a6090c
Krzysztof Socha <ksocha@ksocha.com>
2009-08-08 19:51:50 +0200

    templates update, added some shadows, utils fix

--- utils.py
+++ utils.py
@@ -15,16 +15,17 @@ class TpmRequestHandler(webapp.RequestHandler):
 		webapp.RequestHandler.__init__(self, **kw)
 		self.user = users.get_current_user()
 		self.is_admin = str(self.user) in ADMINS
+		self.profile = None
 		if self.user:
 			self.profile = db.Query(models.Profile).filter("user =", self.user).get()
 
 	def render(self, tmpl, tmplprefix="blog/", *args, **kw):
 		template_values = dict(**kw)
+		template_values.update({'user': self.user})
 		if self.profile:
 			template_values.update({'profile': self.profile})
-		elif not "message" in template_values:
-			 template_values.update({'message': "We strongly recommend you to update your profile <a href=\"/user/profile\">here</a>."})
-		template_values.update({'user': self.user})
+		elif self.user and not "message" in template_values:
+			 template_values.update({'message': "We strongly recommend you to create your profile <a href=\"/user/create\">here</a>."})
 		template_values.update({'users': users})
 		template_values.update({'admin': self.is_admin})
 		path = os.path.join(os.path.dirname(__file__), '%stemplates/%s' % (tmplprefix, tmpl))
@@ -36,13 +37,13 @@ class TpmRequestHandler(webapp.RequestHandler):
 	def misc_render(self, tmpl, *args, **kw):
 		self.render(tmpl, "misc/", *args, **kw)
 
-def error(self, errorcode, message = None, uri = None, referer=None):
+def error(self, errorcode, error_message=None, uri=None, referer=None):
 	errorcodes = (400,401,403,404,500)
 	if not errorcode in errorcodes:
 		return False
 	if 'HTTP_REFERER' in os.environ:
 		referer = os.environ['HTTP_REFERER']
-	self.misc_render("%s.html" % errorcode, message=message, uri=uri, referer=referer)
+	self.misc_render("%s.html" % errorcode, error_message=error_message, uri=uri, referer=referer)
 
 def administrator(method):
     @functools.wraps(method)
